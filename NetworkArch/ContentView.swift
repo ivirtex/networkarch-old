76a335ab5c971c4ab5170bff42fa0665136e3aa0
Hubert Jóźwiak <hjozwiakdx@gmail.com>
2020-07-31 11:41:09 +0200

    Initial Commit

---
+++ NetworkArch/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  NetworkArch
+//
+//  Created by Hubert Jóźwiak on 31/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkArch/NetworkArchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkArchApp.swift
+//  NetworkArch
+//
+//  Created by Hubert Jóźwiak on 31/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct NetworkArchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
