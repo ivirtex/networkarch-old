0dccce36824ec786e6f74e3f1885d2006d856b4f
Hubert Jóźwiak <hjozwiakdx@gmail.com>
2020-10-17 20:22:44 +0200

    Final 1.2b1

---
+++ NetworkArch/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  NetworkArch
+//
+//  Created by Hubert Jóźwiak on 17/10/2020.
+//
+
+import Foundation
+import UIKit
+import Appodeal
+import SwiftyStoreKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Appodeal.disableNetworks(["admob"])
+        AdvertisingProvider.shared.initialize()
+        SKReviewRequest().showReviewView(afterMinimumLaunchCount: 3)
+        SwiftyStoreKit.completeTransactions(atomically: true) { purchases in
+            for purchase in purchases {
+                switch purchase.transaction.transactionState {
+                case .purchased, .restored:
+                    if purchase.needsFinishTransaction {
+                        // Deliver content from server, then:
+                        SwiftyStoreKit.finishTransaction(purchase.transaction)
+                    }
+                // Unlock content
+                case .failed, .purchasing, .deferred:
+                    break // do nothing
+                
+                @unknown default:
+                    print("wtf is that case")
+                }
+            }
+        }
+        return true
+    }
+    
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
